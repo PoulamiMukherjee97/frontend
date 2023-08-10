a2b9aac98a451c61ac90c12504c3d78b90d09a26
PoulamiMukherjee97 <pou.mkjee@gmail.com>
2023-08-10 15:31:38 +0530

    Data updation

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState('');
+  const [data, setData] = useState('Data not received from backend');
   const fetchData = async () => {
-    const res = await axios.get(`${process.env.REACT_APP_URL}`);
-    setData(res.data?.message);
+    try{
+      const res = await axios.get(`${process.env.REACT_APP_URL}`);
+      setData(res.data?.message);
+    } catch(err){
+      console.log(err);
+    }
+    
   }
   useEffect(() => {
-    
+
     fetchData();
   }, [])
   return (
     <div className="App">
+      Client says:
       {data}
     </div>
   );
