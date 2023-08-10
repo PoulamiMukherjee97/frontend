8f7e2c98ecfb04b1f4dd399467ecdcee1d245a75
PoulamiMukherjee97 <pou.mkjee@gmail.com>
2023-08-10 13:57:13 +0530

    connection with backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState('');
+  const fetchData = async () => {
+    const res = await axios.get(`${process.env.REACT_APP_URL}`);
+    setData(res.data?.message);
+  }
+  useEffect(() => {
+    fetchData();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data}
     </div>
   );
 }
